21df03ca62be1da5cf5a1763741c8978835406c3
r-falcon <rfalcon@163.com>
2022-07-12 14:55:59 +0800

    feat: 新页面

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
 <template>
-  <router-view />
+  <div class="main_box">
+    <div class="left_box"></div>
+    <div class="right_box"><router-view /></div>
+  </div>
 </template>
+
+<style lang="scss">
+.main_box {
+  display: flex;
+
+  .left_box {
+    width: 300px;
+  }
+
+  .right_box {
+    flex: 1;
+  }
+}
+</style>
